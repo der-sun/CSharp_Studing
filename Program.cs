5ae63b2a1dda416d06e9314af9b2a854dd4b6a42
der-sun <86686038+der-sun@users.noreply.github.com>
2023-06-04 04:19:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "5.5";
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",//Учитываем "." при конверте, ведь учитывается ","
+            };
+            double dbl = double.Parse(str, numberFormatInfo);//Узконаправленный конверт (из строки в число) + учёт знака
+
+            str = "5.5awdawd";
+            double.TryParse(str, out dbl);//TryParse не кидает исключения, если не получается распарсить, то скипает
+        }
+    }
+}
